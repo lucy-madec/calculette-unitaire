52c122dc69b1ea602de6683926f37ff84ca1e4fc
Lucy <lucy.madec@laplateforme.io>
2025-04-24 11:23:28 +0200

    Refactor code structure for improved readability and maintainability

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -10,4 +10,24 @@ class CalculatorTest extends TestCase
         $calc = new Calculator();
         $this->assertEquals(4, $calc->calculate('2+2'));
     }
+
+    public function testSoustraction()
+    {
+        $calc = new Calculator();
+        $this->assertEquals(2, $calc->calculate('5-3'));
+    }
+
+    public function testMultiplication()
+    {
+        $calc = new Calculator();
+        $this->assertEquals(24, $calc->calculate('6*4'));
+    }
+
+    public function testDivision()
+    {
+        $calc = new Calculator();
+        $this->assertEquals(5, $calc->calculate('10/2'));
+    }
+
+
 }
